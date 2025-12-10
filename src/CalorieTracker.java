72453b307f43b717a4b4ad6c3a6e2452ba8dc990
Dhruvit Sharma <sharma.1408@osu.edu>
2025-12-10 18:46:48 -0500

    Update CalorieTracker.java

--- src/CalorieTracker.java
+++ src/CalorieTracker.java
@@ -1,56 +1,14 @@
-import java.util.Map;
+public interface CalorieTracker {
 
-import components.map.Map1L;
+    void addFood(String name, int calories);
 
-public class CalorieTracker {
+    void removeFood(String name);
 
-    // Map of food items and calorie counts.
+    int getCalories(String name);
 
-    private Map<String, Integer> foodLog;
+    int getTotalCalories();
 
-    //Default constructor.
+    Iterable<String> foods();
 
-    public CalorieTracker() {
-        this.foodLog = new Map1L<>();
-    }
-
-    public void addFood(String name, int calories) {
-        // to store name and calories
-    }
-
-    //calculate total calories
-    public int getTotalCalories() {
-
-        return 0;
-    }
-
-    //Clears the tracker for a new day.
-
-    public void clearDay() {
-
-    }
-
-//main method
-public static void main(String[] args) {
-
-        // CalorieTracker object
-        CalorieTracker tracker = new CalorieTracker();
-
-        // Example data input
-        tracker.addFood("cereal", 150);
-        tracker.addFood("Salad", 400);
-        tracker.addFood("Protein Shake", 250);
-
-        // Display total calories
-        out.println("Total calories today: " + tracker.getTotalCalories());
-
-        // Remove a food
-        tracker.removeFood("cereal");
-
-        // Show updated total
-        out.print("After removing oatmeal:");
-        out.print("Total calories: " + tracker.getTotalCalories());
-
-        // Display all foods eaten today
-        out.println("Foods logged:");
-        tracker.displayFoods(out);
+    void clearDay();
+}
